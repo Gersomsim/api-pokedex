a6b083da5851d528e5bb26058625fdb286f3e0fe
Gersomsim <gersomhernandez@me.com>
2023-07-10 21:45:55 -0600

    :zap: Implementando el DTO

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,19 @@
-export class CreatePokemonDto {}
+import {
+  IsInt,
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
+  name: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @IsPositive()
+  no: number;
+}
