c9dd81ef52cfa90c206dc35c72a9d9ce374f227a
Gersomsim <gersomhernandez@me.com>
2023-07-10 21:52:20 -0600

    :sparkles: Insertando data

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Pokemon } from "./entities/pokemon.entity";
 
 @Injectable()
 export class PokemonService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
   create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
-    return 'This action adds a new pokemon';
+    return this.pokemonModel.create(createPokemonDto);
   }
 
   findAll() {
